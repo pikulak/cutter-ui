1305ca9b38501e1e659b46c8ba5c689c648379d2
Kacper <pikulak1@gmail.com>
2018-06-25 04:07:49 +0200

    Reorganize project structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './styles/index.css';
+import App from './components/App';
+import registerServiceWorker from './workers/ServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
